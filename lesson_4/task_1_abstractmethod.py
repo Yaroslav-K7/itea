ffbb15d98d9f9d28755152bb3f66fe20d3e5dc2c
Yaroslav-K7 <yaroslav.kuznetsov1994@gmail.com>
2020-09-16 21:52:54 +0300

    fix names for methods

--- lesson_4/task_1_abstractmethod.py
+++ lesson_4/task_1_abstractmethod.py
@@ -5,11 +5,11 @@ from datetime import datetime
 class Person(ABC):
 
     @abstractmethod
-    def get_info_about_person(self):
+    def get_info(self):
         pass
 
     @abstractmethod
-    def get_years(self):
+    def get_age(self):
         pass
 
 
@@ -20,10 +20,10 @@ class Enrollee(Person):
         self.date_of_birthday = date_of_birthday
         self.faculty = faculty
 
-    def get_info_about_person(self):
+    def get_info(self):
         return f'Enrollee: {self.surname} has birthday {self.date_of_birthday}, faculty: {self.faculty}'
 
-    def get_years(self):
+    def get_age(self):
         symbols = [',', ';', ':', '/', '-']
         for _ in symbols:
             self.date_of_birthday = self.date_of_birthday.replace(_, '.')
@@ -38,18 +38,19 @@ class Student(Enrollee):
         super().__init__(surname, date_of_birthday, faculty)
         self.course = course
 
-    def get_info_about_person(self):
+    def get_info(self):
         return f'Student: {self.surname} has birthday {self.date_of_birthday}, ' \
                f'faculty: {self.faculty}, course: {self.course}'
 
 
 class Teacher(Enrollee):
+
     def __init__(self, surname, date_of_birthday, faculty, position, experience):
         super().__init__(surname, date_of_birthday, faculty)
         self.position = position
         self.experience = experience
 
-    def get_info_about_person(self):
+    def get_info(self):
         return f'Teacher: {self.surname} has birthday {self.date_of_birthday}, ' \
                f'faculty: {self.faculty}, position: {self.position}, experience: {self.experience}'
 
@@ -59,8 +60,7 @@ list_of_person = [Enrollee('Kuznietsov', '1994:04:07', 'DOIT'),
         Teacher('Spilberg', '1974:05:16', 'DOIT', 'BIOLOGY', 10)]
 
 for x in list_of_person:
-    print(x.get_info_about_person())
-
+    print(x.get_info())
 
 year_from = int(input("year range from: "))
 year_to = int(input("year range to "))
@@ -72,7 +72,7 @@ for i in range(year_from, year_to):
     range_of_years_from_to.append(i)
 
 for z in list_of_person:
-    persons_years.append((z.get_years(), z.get_info_about_person()))
+    persons_years.append((z.get_age(), z.get_info()))
 
 dict_persons_years = dict(persons_years)
 
