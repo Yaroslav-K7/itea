3e0d5dba960bfb5fdc56c62653eae0160d180617
Yaroslav-K7 <yaroslav.kuznetsov1994@gmail.com>
2020-09-22 18:33:28 +0300

    some fix

--- lesson_4/new_era.py
+++ lesson_4/new_era.py
@@ -1,12 +1,41 @@
-import datetime
+from datetime import datetime
+import re
+all_users = [{'login': 'yarik', 'password': '12345Qwe!', 'register_time:': '2020-09-22 18:31:10'}]
 
-login_input = input("Enter login: ")
-password_input = input("Enter password: ")
-repeat_password_input = input("Repeat passport: ")
-post = input("Write a message: ")
+while True:
+    login_input = input("Enter login: ")
+    for i in all_users:
+        if i["login"] == login_input:
+            user_password = i["password"]
+            print("This login already exists")
+
+    password_input = input(
+                "Password requirements:\nOne capital letter, one number and one special character. \n"
+                "Also need the length of the password to be between 8 and 18.\n"
+                "print 'q!' for exit\n"
+                "Enter your password: ")
+    reg = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{8,18}$"
+    match_re = re.compile(reg)
+    res = re.search(match_re, password_input)
+    if res:
+        print("Valid Password")
+
+    elif password_input == "q!":
+        break
+    else:
+        print("Enter correct password")
+
+
+    repeat_password_input = input("Repeat passport: ")
+    post = input("Write a message: ")
+    break
+
+
 
 
 class Auth:
+    ADMIN_LOGIN = "admin"
+    ADMIN_PASSWORD = "adminadmin"
 
     is_admin = False
     is_authorised = False
@@ -18,21 +47,40 @@ class Auth:
         self.post_ = post
 
     def register(self):
-        pass
+        if self.password_ == self.repeat_pass:
+            all_users.append({"login": self.login_, "password": self.password_,
+                              "register_time:": datetime.now().strftime("%Y-%m-%d %H:%M:%S")})
+        else:
+            pass
 
     def login(self):
-        pass
+        for i in all_users:
+            if i["login"] == self.login_ and i["password"] == self.password_:
+                print('yes')
+                self.is_authorised = True
+            elif self.login_ == self.ADMIN_LOGIN and self.password_ == self.ADMIN_PASSWORD:
+                print("admin")
+                self.is_authorised = True
+                self.is_admin = True
+            else:
+                self.register()
 
 
 class User(Auth):
 
-    all_users = [{'login': 'admin', 'password': 'adminadmin'}]
-
-
-class Post:
+    #
+    # class Post:
 
     def post(self):
         pass
 
     def get_users_posts(self):
         pass
+
+
+a = User()
+print(all_users)
+a.login()
+print(all_users)
+a.register()
+print(all_users)
