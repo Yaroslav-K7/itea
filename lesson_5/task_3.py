bbd0f8775bccf5a878d3baa767cd279448aa20d1
Yaroslav-K7 <yaroslav.kuznetsov1994@gmail.com>
2020-09-29 20:46:40 +0300

    review

--- lesson_5/task_3.py
+++ lesson_5/task_3.py
@@ -6,14 +6,14 @@
 ссылке он работает, так же должен сообщать когда скачивание
 закончится."""
 
-
 import time
 from functools import wraps
-from threading import Thread
-import requests
 from random import randint
+from threading import Thread
 
-url = 'https://rcnit.com.ua/wp-content/uploads/2016/10/qa.jpg'
+import requests
+
+one_url = 'https://rcnit.com.ua/wp-content/uploads/2016/10/qa.jpg'
 
 
 def name_of_threads():
@@ -30,29 +30,36 @@ def name_of_threads():
 
 @name_of_threads()
 def ten():
-    save_file = requests.get(url)
+    save_file = requests.get(one_url)
     with open('new_image.jpg', 'wb') as f:
         f.write(save_file.content)
 
 
 "--------------------------------------------------------------------"
-urls = ['https://rcnit.com.ua/wp-content/uploads/2016/10/qa.jpg',
-        'https://images.wallpaperscraft.ru/image/mashina_sportkar_neon_157154_1280x720.jpg',
-        'https://images.wallpaperscraft.ru/image/mashina_sportkar_neon_156622_300x188.jpg',
-        'https://images.wallpaperscraft.ru/image/mashina_sportkar_doroga_156423_300x188.jpg',
-        'https://images.wallpaperscraft.ru/image/rollsroyce_vid_speredi_fary_136993_1280x720.jpg']
+ten_urls = ['https://rcnit.com.ua/wp-content/uploads/2016/10/qa.jpg',
+            'https://images.wallpaperscraft.ru/image/mashina_sportkar_neon_157154_1280x720.jpg',
+            'https://images.wallpaperscraft.ru/image/mashina_sportkar_neon_156622_300x188.jpg',
+            'https://images.wallpaperscraft.ru/image/mashina_sportkar_doroga_156423_300x188.jpg',
+            'https://images.wallpaperscraft.ru/image/rollsroyce_vid_speredi_fary_136993_1280x720.jpg',
+            'https://rcnit.com.ua/wp-content/uploads/2016/10/qa.jpg',
+            'https://images.wallpaperscraft.ru/image/mashina_sportkar_neon_157154_1280x720.jpg',
+            'https://images.wallpaperscraft.ru/image/mashina_sportkar_neon_156622_300x188.jpg',
+            'https://images.wallpaperscraft.ru/image/mashina_sportkar_doroga_156423_300x188.jpg',
+            'https://images.wallpaperscraft.ru/image/rollsroyce_vid_speredi_fary_136993_1280x720.jpg'
+            ]
 
 
 def name_of_threads_():
     def actual_decorator(func):
         @wraps(func)
         def wrapper():
-            for i in urls:
-                x = randint(0, 100)
-                print(f"i : {i}")
-                thread = Thread(target=func(i, x), args=(i,))
+            for url in ten_urls:
+                name = randint(0, 100)
+                print(f"Thread starting, {url}")
+                start = time.time()
+                thread = Thread(target=func(url, name), args=(url,))
                 thread.start()
-                print("end")
+                print(f"Thread end. Time: {time.time() - start}")
 
         return wrapper
 
@@ -60,9 +67,7 @@ def name_of_threads_():
 
 
 @name_of_threads_()
-def ten_(i, x):
-    save_file = requests.get(i)
-    with open(f'new_image{x}.jpg', 'wb') as f:
+def ten_(url, name):
+    save_file = requests.get(url)
+    with open(f'new_image{name}.jpg', 'wb') as f:
         f.write(save_file.content)
-
-ten_()
