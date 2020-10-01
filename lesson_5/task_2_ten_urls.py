af4d864edaf86671e2f8a6ca11250b35edaa6b55
Yaroslav-K7 <yaroslav.kuznetsov1994@gmail.com>
2020-10-01 13:04:34 +0300

    fix

--- lesson_5/task_2_ten_urls.py
+++ lesson_5/task_2_ten_urls.py
@@ -8,19 +8,16 @@ import requests
 one_url = 'https://rcnit.com.ua/wp-content/uploads/2016/10/qa.jpg'
 
 
-def name_of_threads():
-    def actual_decorator(func):
-        @wraps(func)
-        def wrapper():
-            thread = Thread(target=func)
-            thread.start()
+def name_of_threads(func):
+    @wraps(func)
+    def wrapper():
+        thread = Thread(target=func)
+        thread.start()
 
-        return wrapper
-
-    return actual_decorator
+    return func
 
 
-@name_of_threads()
+@name_of_threads
 def ten():
     save_file = requests.get(one_url)
     with open('new_image.jpg', 'wb') as f:
@@ -41,22 +38,19 @@ urls = ['https://rcnit.com.ua/wp-content/uploads/2016/10/qa.jpg',
         ]
 
 
-def name_of_threads_():
-    def actual_decorator(func):
-        def wrapper(*args, **kwargs):
-            print(f"Thread starting - {args}")
-            start = time.time()
-            Thread(target=func, args=args, kwargs=kwargs).start()
-            print(f"Thread end. Time: {time.time() - start}")
+def name_of_threads_(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        print(f"Thread starting - {args}")
+        start = time.time()
+        Thread(target=func, args=args, kwargs=kwargs).start()
+        print(f"Thread end. Time: {time.time() - start}")
 
-            return func
-
-        return wrapper
-
-    return actual_decorator
+        return func
+    return wrapper
 
 
-@name_of_threads_()
+@name_of_threads_
 def download_and_save(url, name):
     save_file = requests.get(url)
     with open(name, 'wb') as f:
@@ -65,5 +59,3 @@ def download_and_save(url, name):
 
 for u in urls:
     download_and_save(u, f'{randint(0, 1000)}.jpg')
-
-
